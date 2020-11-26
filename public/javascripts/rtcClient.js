31188968c367320c9fc4c3f892d941e0ef597041
lukelu0520 <appl.chlu@gmail.com>
2020-11-25 21:34:29 -0800

    fixed ice server usrname & credential

--- public/javascripts/rtcClient.js
+++ public/javascripts/rtcClient.js
@@ -4,24 +4,11 @@ var PeerManager = (function () {
       config = {
         peerConnectionConfig: {
           iceServers: [
-            // {urls: ['stun:stun.l.google.com:19302', 'stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302', 
-            // 'stun:stun3.l.google.com:19302', 'stun:stun4.l.google.com:19302']
-            // },
-            // {
-            //   urls: 'turn:192.158.29.39:3478?transport=udp',
-            //   credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            //   username: '28224511:1379330808'
-            // },
-            // {
-            //   urls: 'turn:192.158.29.39:3478?transport=tcp',
-            //   credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            //   username: '28224511:1379330808'
-            // },
-            //{urls:'stun:stun.l.google.com:19302'},
+            {urls:'stun:stun.l.google.com:19302'},
             {
-              urls: 'turn:34.94.159.96',
-              credential: 'tourmato',
-              username: '1314520'
+              urls: 'turn:tourmato.homes:3478',
+              username: 'tourmato',
+              credential: '1314520'
             }
           ]
         },
